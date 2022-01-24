7b5caa2f4d2d5ab2f74b05f79f4cee2f95b05159
Michał <michalmalicki@op.pl>
2022-01-24 17:33:35 +0100

    my version magic8ball

---
+++ js/main.js
@@ -0,0 +1,33 @@
+const ballImg = document.querySelector('img');
+const input = document.querySelector('input');
+const answer = document.querySelector('.answer');
+const error = document.querySelector('.error');
+const answerArray = ['Nie chcesz znać odpowiedzi na to pytanie...:/','Nie.' ,'Tak!','Nie wiem' , 'Może']
+
+const checkInput = () => {
+    ballImg.classList.remove('shake-animation');
+     if(input.value === ''){
+        error.textContent = 'Musisz zadać jakieś pytanie!';
+        answer.textContent = '';
+    }else if(input.value.slice(-1).includes('?') === false){
+        error.textContent = 'Pytanie musi być zakonczone znakiem "?"';
+        answer.textContent = '';
+    }else {
+        error.textContent = '';
+        randomAnswer();
+    }
+}
+
+const startAnimation = () => {
+    ballImg.classList.add('shake-animation');
+    setTimeout(checkInput,1000);
+}
+
+const randomAnswer = () => {
+    let randomNumber = Math.floor(Math.random() * answerArray.length);
+    answer.textContent = answerArray[randomNumber]
+}
+
+
+
+ballImg.addEventListener('click', startAnimation)
